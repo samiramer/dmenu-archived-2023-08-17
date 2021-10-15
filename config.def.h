be427cc50a662b563e0d300a8e18b8cdd92aff49
samer <samirmamer@gmail.com>
2021-10-15 08:25:29 -0400

    Use gruvbox colors

--- config.def.h
+++ config.def.h
@@ -5,15 +5,15 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Ubuntu:size=10"
+	"SaucCodePro Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#f8f8f2", "#282a36" },
-	[SchemeSel] = { "#f8f8f2", "#44475a" },
-	[SchemeSelHighlight] = { "#ff79c6", "#44475a" },
-	[SchemeNormHighlight] = { "#ff79c6", "#282a36" },
+	[SchemeNorm] = { "#fbf1c7", "#282828" },
+	[SchemeSel] = { "#fbf1c7", "#3c3836" },
+	[SchemeSelHighlight] = { "#fe8019", "#3c3836" },
+	[SchemeNormHighlight] = { "#fe8019", "#282828" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
